cf8e3ce58c4ad6e6d9ac5a8b948d4122dd1d3d4e
frontendokulu <frontendokulu@gmail.com>
2020-04-16 18:51:21 +0300

    nodemon silindi, pug işlemleri eklendi

--- express/pugUyg/index.js
+++ express/pugUyg/index.js
@@ -6,6 +6,12 @@ app.set('view engine', 'pug');
 app.get('/', (req,res ) => {
     res.render('index',{isim:"Avatar Aang", yetenek : "Hava Bükücü" });
 });
+app.get('/anasayfa', (req,res ) => {
+    res.render('anasayfa');
+});
+app.get('/iletisim', (req,res ) => {
+    res.render('iletisim');
+});
 
 app.listen(3000, () =>{
     console.log("express çalıştı");
