68b8da6f676f78d27c7388a491131a4245048999
frontendokulu <frontendokulu@gmail.com>
2020-04-16 13:05:06 +0300

    Express öncesi dersler, temel_egitim klasörüne alındı

--- moduleExport_require.js
+++
@@ -1,6 +0,0 @@
-// const foo = require('./my_modules/modul1').foo;
-// const bar = require('./my_modules/modul1').bar;
-// Üstteki 2 satır yerine
-    const {foo, bar} =require('./my_modules/modul1');
-console.log(foo());
-console.log(bar());
